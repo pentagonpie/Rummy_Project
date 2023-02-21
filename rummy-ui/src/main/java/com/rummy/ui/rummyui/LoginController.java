eb067d9a9be37affe1f15b4cfe74e88e6ce11307
Tom Arbuz <pentagonpie@gmail.com>
2023-02-21 14:21:52 +0200

    login screen leads to main screen, main screen goes back to login screen via logout button

--- rummy-ui/src/main/java/com/rummy/ui/rummyui/LoginController.java
+++ rummy-ui/src/main/java/com/rummy/ui/rummyui/LoginController.java
@@ -1,11 +1,17 @@
 package com.rummy.ui.rummyui;
 
+import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class LoginController {
     private final RMIClient rmiClient;
@@ -19,6 +25,7 @@ public class LoginController {
 
     @FXML
     protected TextField password;
+    private Button btnLogin;
 
     @FXML
     protected void onLoginClick() {
@@ -27,9 +34,33 @@ public class LoginController {
         final Boolean loginResult = this.rmiClient.login(username, password);
 
         if (loginResult) {
+
+        try {
+            //Parent fxmlLoader = FXMLLoader.load(getClass().getResource("mainScreen.fxml"));
+            FXMLLoader  fxmlLoader = new FXMLLoader(RummyApplication.class.getResource("mainScreen.fxml"));
+            Scene scene;
+            System.out.println("testing1");
+            //scene = new Scene(fxmlLoader);
+            Stage primaryStage = new Stage();
+            primaryStage.setScene(new Scene(fxmlLoader.load()));
+            primaryStage.setTitle("Welcome to Rummy!");
+            
+            //primaryStage.initOwner(btnLogin.getScene().getWindow());
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
             Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-            alert.setTitle("Login succeeded");
-            alert.setHeaderText("Welcome " + username);
+            alert.setTitle("IOException");
+            alert.setHeaderText("Exception at login screen controller");
+            alert.show();
+        }
+            
+            
+
+        }else{
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Login Failed");
+            alert.setHeaderText("try again");
             alert.show();
         }
     }
--- rummy-ui/src/main/java/com/rummy/ui/rummyui/MainScreenController.java
+++ rummy-ui/src/main/java/com/rummy/ui/rummyui/MainScreenController.java
@@ -4,23 +4,54 @@
  */
 package com.rummy.ui.rummyui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
  *
  * @author pentagonpie
  */
-public class MainScreenController implements Initializable {
+public class MainScreenController  {
 
     /**
      * Initializes the controller class.
      */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+    private Button logout;
     
-}
+    @FXML
+    protected void onLogoutClick()  {
+        FXMLLoader fxmlLoader = new FXMLLoader(RummyApplication.class.getResource("login.fxml"));
+        Scene scene;
+        try {
+            scene = new Scene(fxmlLoader.load(), 320, 240);
+            Stage primaryStage = new Stage();
+            primaryStage.setTitle("Welcome to Rummy!");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException ex) {
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("IOException");
+            alert.setHeaderText("Exception at main screen controller");
+            alert.show();
+        }
+
+    }
+    }
+    
+    
+  //  @Override
+   // public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    //}    
+    
+
